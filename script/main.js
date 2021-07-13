c29742a07867b2d235b3cfa7fe0ad1d9764a4780
sofiarnunes <sofia.ri.nunes@gmail.com>
2021-07-13 17:06:26 +0100

    Add navbar and hero section

---
+++ script/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        autoplay: true,
+        arrows: false,
+    });
+    
+});
